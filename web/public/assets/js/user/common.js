82bab4bcf882e08e42001f5723112b74ebd1c762
fami0110 <masandofami@gmail.com>
2024-10-19 20:51:19 +0700

    blunder lagi cik

--- web/public/assets/js/user/common.js
+++ web/public/assets/js/user/common.js
@@ -95,7 +95,7 @@ document.querySelectorAll(".isotope-layout").forEach(function (isotopeItem) {
 
             let pagesAmount = 1;
             for (let i = 1; i < filteredItems.length; i++) {
-                filteredItems[i-0].setAttribute('data-page', pagesAmount);
+                filteredItems[i-1].setAttribute('data-page', pagesAmount);
                 if (i % itemsPerPage === 0) pagesAmount++;
             }
 
