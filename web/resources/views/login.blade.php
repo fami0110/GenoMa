3996b1e54300951e977dbb58c5b53e5f68c3cc30
fami0110 <masandofami@gmail.com>
2024-10-20 14:38:48 +0700

    try og:image FIX

--- web/resources/views/login.blade.php
+++ web/resources/views/login.blade.php
@@ -9,7 +9,7 @@
     <title>GenoMa - Login</title>
 
     <meta name="description" content="Get to Know Malang: {{ __('meta-description') }}" />
-    <meta property="og:image" content="{{ asset('assets/img/thumbnail.jpg') }}">
+    <meta property="og:image" content="{{ asset('assets/img/genoma.jpg') }}">
     <meta name="keywords" content="Malang, Malang City, Kota Malang, budaya, pariwisata, UMKM, kuliner, Indonesia, Jawa Timur " />
     <meta name="author" content="GenoMa's Team">
 
--- web/resources/views/template/header.blade.php
+++ web/resources/views/template/header.blade.php
@@ -18,7 +18,7 @@
     <title>GenoMa - {{ $prefix . __("title-$current_page") }}</title>
 
     <meta name="description" content="Get to Know Malang: {{ __('meta-description') }}" />
-    <meta property="og:image" content="{{ asset('assets/img/thumbnail.jpg') }}">
+    <meta property="og:image" content="{{ asset('assets/img/genoma.jpg') }}">
     <meta name="keywords" content="Malang, Malang City, Kota Malang, budaya, pariwisata, UMKM, kuliner, Indonesia, Jawa Timur " />
     <meta name="author" content="GenoMa's Team">
 
